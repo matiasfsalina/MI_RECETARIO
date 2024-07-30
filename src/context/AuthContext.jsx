ff4e7dde98c16733ed91415d074933adb15445bb
Matias F Salina <salinamatiasf@gmail.com>
2024-07-30 10:22:37 -0300

    se implemento AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+// Componente que define el contexto de autenticación y proporciona los hooks 
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem('token', token);
+    } else {
+      localStorage.removeItem('token');
+    }
+  }, [token]);
+
+  const login = (newToken) => setToken(newToken);
+  const logout = () => setToken('');
+
+    const isAuthenticated = !!token;  // Determina si el usuario está autenticado
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
